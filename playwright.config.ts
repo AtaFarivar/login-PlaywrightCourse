6d47febbe9b4989e79331931f2b26a5cfa51dae1
AtaFarivar <87602544+AtaFarivar@users.noreply.github.com>
2026-09-03 17:41:57 +0300

    all browsers activated and changed in paralel run in config

--- playwright.config.ts
+++ playwright.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
 
-    /* {
+    {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
@@ -56,6 +56,6 @@ export default defineConfig({
     {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
-    }, */
+    },
   ],
 });
